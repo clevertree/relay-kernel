da177612b1312a752efaaadc276c97c3a5c83922
Ari Asulin <ari.asulin@gmail.com>
2017-09-13 11:21:49 -0700

    select all save map

--- tests/files/game1/sprite/player1.sprite.js
+++ tests/files/game1/sprite/player1.sprite.js
@@ -23,8 +23,8 @@
 
         // Sprite Sheet
         var fSpriteSheet = new Fragment.SpriteSheet(gl, DIR_SHEET_LEM, SPRITE_RESOLUTION, SPRITE_RESOLUTION, (1/16 * 1000));
-        setScale(0.1, 0.1, 0.1);
-        move(-6, 8, 0);
+        // setScale(0.1, 0.1, 0.1);
+        // move(-6, 8, 0);
 
 
         /**
